9a25d1a0bf817de17b7b87d5827a2a64a52272ac
ademyalcin27 <ademyalcin27@gmail.com>
2020-06-08 23:54:48 +0300

    8 Vuetify Theme and Font Configuration

--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -1,6 +1,22 @@
+// eslint-disable-next-line
+import '@mdi/font/css/materialdesignicons.css'; // Ensure you are using css-loader
 import Vue from 'vue';
-import Vuetify from 'vuetify/lib';
+import Vuetify, { colors } from 'vuetify/lib';
 
 Vue.use(Vuetify);
 
-export default new Vuetify({});
+export default new Vuetify({
+  icons: {
+    iconfont: 'mdi', // default - only for display purposes
+  },
+  theme: {
+    themes: {
+      dark: {
+        primary: colors.pink.darken1,
+      },
+      light: {
+        primary: '#fff',
+      },
+    },
+  },
+});
